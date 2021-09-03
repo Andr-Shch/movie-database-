3e449e254e304b9de114e2ec16c8da551654b5c9
Andr-Shch <74563519+cybertrip-glitch@users.noreply.github.com>
2021-09-03 11:19:41 +0300

    pre final

--- src/components/MovieInfo/moveiInfo.styles.js
+++ src/components/MovieInfo/moveiInfo.styles.js
@@ -1,8 +1,33 @@
 import styled from "styled-components";
+import { BACKDROP_SIZE, IMAGE_BASE_URL } from "../../config";
 
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  background: ${({backdrop})=> 
+    backdrop ? `url('${IMAGE_BASE_URL}${BACKDROP_SIZE}${backdrop}')` : '#000'
+  };
+  
+  background-size: cover;
+  background-position:center;
+  padding: 40px 20px;
+  animation: animateMovieInfo 1s ;
 
-export const Content = styled.div``;
+  @keyframes animateMovieInfo{
+      from{
+          opacity: 0;
+      }
+      to{
+          opacity: 1;
+      }
+  }
+
+
+
+`;
+
+export const Content = styled.div`
+ display: flex;
+ 
+`;
 
 export const Text = styled.div``
